8c74eed2c98267ef1dd2d9363d3eaf2fb35cf81f
yellow submarine <me@patmalins.dev>
2021-11-26 18:48:53 +0000

    added dependancies + components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './app.scss'
+import Navbar from './Components/0-Navbar/Navbar';
+import Home from './Components/1-Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <div className="sections">
+        <Home/>
+      </div>
     </div>
   );
 }
