515ace537da54a074c5883f03fac422d831ac1da
yellow submarine <me@patmalins.dev>
2021-12-09 13:10:43 +0000

    changed menu layout

--- src/Components/0-Navbar/Navbar.jsx
+++ src/Components/0-Navbar/Navbar.jsx
@@ -3,20 +3,18 @@ import './navbar.scss';
 export default function Navbar() {
     return (
         <div className="navbar">
-                <ul className='navbar-items'>
-                    <a className='navbar-logo' href="#home">
-                        <img src="Assets/NavLogo.png" alt="Logo" />
-                    </a>
-                    <a href="#home">
-                        <li>Home</li>
-                    </a>
-                    <a href="about">
-                        <li>About</li>
-                    </a>
-                    <a href="contact">
-                        <li>contact</li>
-                    </a>
-                </ul>
+        <div className='left'>
+            <a className='navbar-logo' href="#home">
+                <img src="Assets/NavLogo.png" alt="Logo" />
+            </a>
+        </div>
+        <div className="right">
+            <ul className='menu'>
+                <a href="#home"><li>Home</li></a>
+                <a href="about"><li>About</li></a>
+                <a href="contact"><li>contact</li></a>
+            </ul>
+        </div>
         </div>
     )
 }
